b7c8dc6e0865debf099ac4ad18aef3412d88ef68
Pragunie Aditya(ray) <150484798+CipherEnigma@users.noreply.github.com>
2025-10-06 16:45:18 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (58.24%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,9 +3,11 @@ public:
     int singleNumber(vector<int>& nums) {
         //xor operation with bits
         // 10^10 will give 00
+        //n^n=0
+        //n^0=n
         int ans=0;
         for(int i: nums){
-            ans=ans^i;
+            ans=ans^i; //
         }
 
         return ans;
