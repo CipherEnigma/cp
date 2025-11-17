2138ee99e1585fe30f397e5b7853a96bf147f129
Pragunie Aditya(ray) <150484798+CipherEnigma@users.noreply.github.com>
2025-11-17 16:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (81.55%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0)
+        return false;
+        else if(n==1)
+        return true;
+        while(n%4==0){
+            n/=4;
+        }
+
+        return n==1;
+        
+    }
+};
