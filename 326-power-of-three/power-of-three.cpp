ef0acd9bc894dfb6996fbfc445a47124bc96bbdc
Pragunie Aditya(ray) <150484798+CipherEnigma@users.noreply.github.com>
2025-11-17 16:35:47 +0530

    Time: 6 ms (24.81%) | Memory: 8.8 MB (76.64%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+
+        
+        
+        if(n<=0) return false;
+        if(n==1) return true;
+
+       while(n%3==0){
+        n/=3;
+
+       }
+
+       return n==1;//check if it completely divides or not r
+       
+        
+    }
+};
