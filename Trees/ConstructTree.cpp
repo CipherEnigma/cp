12594cfb88ac81073f4bd7ee52eb8d8bff2a07b8
Pragunie Aditya(ray) <150484798+CipherEnigma@users.noreply.github.com>
2026-01-27 22:53:17 +0530

    Implement basic binary tree structure in C++

---
+++ Trees/ConstructTree.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+int data;
+struct Node* left;
+struct Node* right;
+
+Node(int val){
+data=val;
+  left=right=NULL;
+}
+};
+
+int main(){
+  struct Node* root=new Node(1);
+  root->left=new Node(2);
+  root->right=new Node(3);
+
+  root->left->right=new Node(4);
+
+  //cout<<root<<endl; this wll give only a random address
+  cout<<root->data<<endl;
+  
+
+}
